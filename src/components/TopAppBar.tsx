fc9fb5df91843e8dce4590e8132b7633016f47b3
rubicus3 <laba625@yandex.ru>
2025-05-26 02:36:26 +0300

    list

--- src/components/TopAppBar.tsx
+++ src/components/TopAppBar.tsx
@@ -11,21 +11,24 @@ export default function TopAppBar() {
 
     return (
         <div className="app-bar">
-            <img
-                src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/d8YN87RSkr/zqi9qpyw_expires_30_days.png"}
-                className="music-image"
-            />
-            <div style={{
-                flexShrink: 0,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-                marginRight: "13px",
-            }}>
-                <span className="font-display-semibold-white">{"Голос победы"}</span>
-                <span className="app-bar-text">{"Музыкальное наследие"}</span>
+            <div style={{display:"flex", flexDirection:"row"}}>
+                <img
+                    src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/d8YN87RSkr/zqi9qpyw_expires_30_days.png"}
+                    className="music-image"
+                />
+                <div style={{
+                    flexShrink: 0,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                    marginRight: "13px",
+                }}>
+                    <span className="font-display-semibold-white">{"Голос победы"}</span>
+                    <span className="app-bar-text">{"Музыкальное наследие"}</span>
+                </div>
             </div>
             <div className="filler"></div>
+            
             <div className="app-bar-button-container"
                 onClick={() => navigate("/archive")}
                 >
